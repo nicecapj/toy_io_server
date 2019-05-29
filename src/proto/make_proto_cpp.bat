d51c179cdda42cd8256839984bcdb11f7b594f61
KimJeLyoung <nicecapj@gmail.com>
2019-05-30 01:56:53 +0900

    serperated returncode from login.proto

--- src/proto/make_proto_cpp.bat
+++ src/proto/make_proto_cpp.bat
@@ -1,1 +1,2 @@
-protoc --cpp_out=. login.proto
+protoc --cpp_out=. returncode.proto
+protoc --cpp_out=. login.proto 
--- src/proto/make_proto_go.bat
+++ src/proto/make_proto_go.bat
@@ -1,1 +1,2 @@
-protoc --go_out=. login.proto
+protoc --go_out=. returncode.proto
+protoc --go_out=. login.proto 
